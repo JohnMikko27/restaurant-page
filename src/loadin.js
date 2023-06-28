e3286fdb7f56badd290a5f0fdcf82b12a54d3d87
Mikko <realmiks27@gmail.com>
2023-06-28 10:59:43 -0700

    Initial commit

---
+++ src/loadin.js
@@ -0,0 +1,25 @@
+import './styles.css'
+
+export const loadIn = () => {
+    const body = document.querySelector('body');
+
+    //create a navbar
+    //with 3 child divs
+    //the middle child div has 4 children divs
+    //append this to body
+
+    const nav = document.createElement('nav');
+    const div = document.createElement('div');
+    const div2 = document.createElement('div');
+    const div3 = document.createElement('div');
+
+    
+
+
+
+
+    
+    //create a message div
+    //put an image to the right of that
+    //append both to #content
+}
